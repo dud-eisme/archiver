2679138abf346ec0b34262d7da3d9a899c8dfe9e
Joe <joe@example.com>
2026-05-15 18:13:30 +0530

    filename inclusion instead of path inclusion

--- zipper.cpp
+++ zipper.cpp
@@ -6,7 +6,8 @@ void zipper(int n_files, char *files[], std::string archive_name) {
   for (int i = 1; i < n_files; i++) {
     std::ifstream file(files[i + 2]);
     std::string line;
-    archive << "[file] " << files[i + 2] << std::endl;
+    std::filesystem::path file_path = files[i + 2];
+    archive << "[file] " << file_path.filename().string() << std::endl;
     while (std::getline(file, line))
       archive << line << std::endl;
     file.close();
